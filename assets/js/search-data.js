0d0b3348ffc9d68e750ad312a5e9278d5ac926ab
jiayi-19 <107102961+jiayi-19@users.noreply.github.com>
2025-03-07 04:55:22 +0000

    Deploying to gh-pages from @ jiayi-19/jiayi-19.github.io@3664b630fa5ad4e5f669310c23aa5595fd990b3d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -71,16 +71,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-our-paper-amoeballm-are-accepted-by-neurips-2024",
+          title: 'Our Paper AmoebaLLM are accepted by NeurIPS 2024!',
           description: "",
           section: "News",},{
         id: 'social-email',
